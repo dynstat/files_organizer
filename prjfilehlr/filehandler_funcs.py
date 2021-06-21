475af0137ab911530e55709807b89672b689872e
dynstat <vivek15@gmx.com>
2021-06-22 00:51:34 +0530

    file organizer project

---
+++ prjfilehlr/filehandler_funcs.py
@@ -0,0 +1,80 @@
+import os
+import shutil
+
+# cwd = os.getcwd()
+# src_path = cwd
+text_path = "C:/Users/vivek/Downloads/def_fldr/textwala"
+video_path = "C:/Users/vivek/Downloads/def_fldr/videowala"
+img_path = "C:/Users/vivek/Downloads/def_fldr/imgwala"
+app_setup_path = "C:/Users/vivek/Downloads/def_fldr/softfileswala"
+docs_path = "C:/Users/vivek/Downloads/def_fldr/docswala"
+audio_path = "C:/Users/vivek/Downloads/def_fldr/audiowala"
+
+docs_extensions = ['pdf', 'doc', 'docx', 'ppt', 'PDF', 'PPT']
+audio_extensions = ['mp3', 'aac', 'flac', 'wav']
+video_extensions = ['mp4', 'mkv', '3gp', 'gif']
+img_extensions = ['jpg', 'jpeg', 'png']
+app_setup_extensions = ['exe', 'zip', 'rar', 'apk', 'msi', '7z']
+text_extensions = ['txt']
+
+
+def filemover(ext, item, from_folder):
+    # print("ye aya hai call hoke = ", ext, '\n')
+    if ext != "folder":
+        if ext in audio_extensions:
+            shutil.move(from_folder+r"\\"+item, audio_path)
+            # print(ext)
+        elif ext in video_extensions:
+            shutil.move(from_folder+r"\\"+item, video_path)
+            # print(ext)
+        elif ext in docs_extensions:
+            shutil.move(from_folder+r"\\"+item, docs_path)
+            # print("ye in docs wala = ", ext, '\n')
+        elif ext in img_extensions:
+            shutil.move(from_folder+r"\\"+item, img_path)
+
+        elif ext in app_setup_extensions:
+            shutil.move(from_folder+r"\\"+item, app_setup_path)
+
+        elif ext in text_extensions:
+            shutil.move(from_folder+r"\\"+item, text_path)
+        else:
+            pass
+
+
+def filecopy(ext, item, from_folder):
+    # print("ye aya hai call hoke = ", ext, '\n')
+    if ext != "folder":
+        if ext in audio_extensions:
+            r = shutil.copy(from_folder+r"\\"+item, audio_path)
+            print(f"ext = {ext} and path = {r}")
+        elif ext in video_extensions:
+            shutil.copy(from_folder+r"\\"+item, video_path)
+            # print(ext)
+        elif ext in docs_extensions:
+            shutil.copy(from_folder+r"\\"+item, docs_path)
+            # print("ye in docs wala = ", ext, '\n')
+        elif ext in img_extensions:
+            shutil.copy(from_folder+r"\\"+item, img_path)
+
+        elif ext in app_setup_extensions:
+            shutil.copy(from_folder+r"\\"+item, app_setup_path)
+
+        elif ext in text_extensions:
+            shutil.copy(from_folder+r"\\"+item, text_path)
+        else:
+            pass
+
+
+def extenChecker(abs_path):
+    counter = 0
+    rev_path = abs_path[::-1]
+    for ch in rev_path:
+        if ch != '.':
+            counter += 1
+        else:
+            break
+    if counter < 5:
+        return rev_path[:counter:1][::-1]
+    else:
+        return "folder"
---
+++ prjfilehlr/filesHandler.py
@@ -0,0 +1,45 @@
+import os
+# import shutil
+import filehandler_funcs as func
+import time
+
+
+func.app_setup_path = "C:/Users/vivek/Desktop/soft"
+func.text_path = "C:/Users/vivek/Desktop/text"
+func.text_path = "C:/Users/vivek/Desktop/audio"
+
+
+
+print("Have you set the paths of each destination folder ? (y/n)")
+if input() != 'y':
+	print("Please set the paths first, Exiting.....!!! ")
+	time.sleep(1)
+	exit()
+
+
+
+while True:
+    cwd = os.getcwd()   # to get the current working directory
+    # returns a list of all the folders and files name
+    # print(cwd)  # C:\Users\vivek\Downloads\def_fldr\prjfilehlr
+    BASE_FOLDER = os.path.dirname(cwd)
+    # print(f" base folder = {BASE_FOLDER}")  # C:\Users\vivek\Downloads\def_fldr
+    list_of_files = os.listdir(BASE_FOLDER)
+
+    for item in list_of_files:
+        # abs_item_path = f"{cwd}/{item}"
+
+        ext = func.extenChecker(item)
+        # print("item = ", item)  #name of file or folder
+        # print(f"ext = {ext}")   #its extension or folder
+        try:
+            func.filecopy(ext, item, BASE_FOLDER)
+        except:
+            pass
+    time.sleep(2)
+    # movedToPath = func.filemover(ext)
+    # print(f"file is successfuly moved to {movedToPath}")
+
+
+# shutil.copy(src=BASE_FOLDER+r"\\"+item, dst=func.mp3_path)
+# shutil.copy(src=BASE_FOLDER+r"\\"+list_of_files[1], dst=func.mp3_path)
