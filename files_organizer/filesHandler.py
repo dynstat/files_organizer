f9908f7e5fd3271590033ae1688ce9687cb3f060
dynstat <vivek15@gmx.com>
2022-02-16 01:42:55 +0530

    changed comments

--- files_organizer/filesHandler.py
+++ files_organizer/filesHandler.py
@@ -27,14 +27,16 @@ while True:
         # abs_item_path = f"{cwd}/{item}"
 
         ext = func.extenChecker(item)
-        i# print("item = ", item)  #name of file or folder
+        # print("item = ", item)  #name of file or folder
         # print(f"ext = {ext}")   #its extension or folder
         try:
             func.filecopy(ext, item, BASE_FOLDER)
         except Exception as e:
-        print("Oops!", e.__class__, "occurred")
-    
+            print("Oops!", e.__class__, "occurred")
+
     time.sleep(2)
+
+    # Use filemover function to move the files (use carefully)
     # movedToPath = func.filemover(ext)
     # print(f"file is successfuly moved to {movedToPath}")
 
